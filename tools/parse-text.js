710e372888d4f2a56ab19aed1fd0798f6012bb6a
gui machiavelli <hey@guimachiavelli.com>
2016-02-14 20:09:07 +0100

    move data inside public folder

--- tools/parse-text.js
+++ tools/parse-text.js
@@ -24,14 +24,14 @@ function writeParsedContent(unparsedContent) {
     const chunks = fragmentParsedContent(parsed);
 
     chunks.forEach(function(chunk, i) {
-        fs.writeFileSync(`./data/chunk-${i}.txt`, chunk);
+        fs.writeFileSync(`./public/data/chunk-${i}.txt`, chunk);
     });
 }
 
 function main() {
     const args = process.argv.slice(2),
           partial = args.indexOf('--partial'),
-          content = fs.readFileSync('./data/bible.txt', { encoding: 'utf8' });
+          content = fs.readFileSync('./public/data/bible.txt', { encoding: 'utf8' });
 
     if (partial > -1) {
         writeParsedContent(content.substr(0, 50000));
